76c43788bdd2e12b7562009a9d5416fe5f234ddd
anakinliv <anakinliv@126.com>
2020-04-20 19:37:47 +0800

    创建工程

---
+++ GameOfLife/Assets/Scripts/GameOfLife.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOfLife : MonoBehaviour
+{
+    public static GameOfLife instance;
+
+    public bool isLoop;
+
+    public int column;
+    public int row;
+
+    public GameObject CellPrefab;
+
+    public Dictionary<Vector2Int, Cell> Cells;
+
+    private void Awake()
+    {
+        Cells = new Dictionary<Vector2Int, Cell>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Init()
+    {
+        Cells.Clear();
+        for (int i = 0; i < column; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                var go = GameObject.Instantiate(CellPrefab);
+                go.transform.position = Vector3.zero;
+                var cellScript = go.GetComponent<Cell>();
+                Vector2Int pos = new Vector2Int(i, j);
+                Cells[pos] = cellScript;
+            }
+        }
+    }
+    
+}
---
+++ GameOfLife/Assets/Scripts/Grid/Cell.cs
@@ -0,0 +1,20 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+
+ public class Cell : MonoBehaviour
+{
+    public bool isAlive;
+    public Dictionary<Vector2Int, Cell> neighbours;
+
+    public Vector2Int pos;
+
+    public Image image;
+
+    public void Generate()
+    {
+        
+    }
+    
+}
