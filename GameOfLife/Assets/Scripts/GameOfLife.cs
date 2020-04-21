ec8509543c68e7337f25e419a0103db6b1ee59ce
anakinliv <anakinliv@126.com>
2020-04-21 18:33:35 +0800

    跑通了一版

--- GameOfLife/Assets/Scripts/GameOfLife.cs
+++ GameOfLife/Assets/Scripts/GameOfLife.cs
@@ -3,6 +3,7 @@ using System.CodeDom.Compiler;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOfLife : MonoBehaviour
 {
@@ -16,6 +17,8 @@ public class GameOfLife : MonoBehaviour
     public GameObject CellPrefab;
 
     public Dictionary<Vector2Int, Cell> Cells;
+    public Transform root;
+    private float updateTime;
 
     private void Awake()
     {
@@ -31,21 +34,37 @@ public class GameOfLife : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > updateTime)
+        {
+            foreach (var cell in Cells.Values)
+            {
+                cell.Generate();
+            }
+            foreach (var cell in Cells.Values)
+            {
+                cell.Doa();
+            }
+
+            updateTime += 0.3f;
+        }
     }
 
     void Init()
     {
         Cells.Clear();
+        GridLayoutGroup glg = root.GetComponent<GridLayoutGroup>();
+        glg.constraintCount = column;
+        glg.cellSize = new Vector2(500 / column, 500 / row);
         for (int i = 0; i < column; i++)
         {
             for (int j = 0; j < row; j++)
             {
                 var go = GameObject.Instantiate(CellPrefab);
-                go.transform.position = Vector3.zero;
+                go.transform.SetParent(root);
                 var cellScript = go.GetComponent<Cell>();
                 Vector2Int pos = new Vector2Int(i, j);
                 Cells[pos] = cellScript;
+                cellScript.Init(this,pos);
             }
         }
     }
--- GameOfLife/Assets/Scripts/Grid/Cell.cs
+++ GameOfLife/Assets/Scripts/Grid/Cell.cs
@@ -1,20 +1,102 @@
-﻿ using System.Collections;
+﻿ using System;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
  using UnityEngine.UI;
+ using Random = UnityEngine.Random;
 
  public class Cell : MonoBehaviour
 {
     public bool isAlive;
+    private bool nextAlive;
     public Dictionary<Vector2Int, Cell> neighbours;
 
     public Vector2Int pos;
 
     public Image image;
 
+    public GameOfLife gameOfLife;
+
     public void Generate()
     {
-        
+        int otherAlive = 0;
+        foreach (var other in neighbours)
+        {
+            if (other.Value.isAlive)
+            {
+                otherAlive++;
+            }
+        }
+
+        if (otherAlive == 3)
+        {
+            nextAlive = true;
+        }
+        else if (otherAlive == 2)
+        {
+            nextAlive = isAlive;
+        }
+        else
+        {
+            nextAlive = false;
+        }
+    }
+
+    public void Doa()
+    {
+        isAlive = nextAlive;
+        if (isAlive)
+        {
+            image.color = Color.black;
+        }
+        else
+        {
+            image.color = Color.white;
+        }
+    }
+
+    public void Init(GameOfLife gameOfLife,Vector2Int pos)
+    {
+        this.gameOfLife = gameOfLife;
+        this.pos = pos;
+        neighbours = new Dictionary<Vector2Int, Cell>();
+        foreach (var other in gameOfLife.Cells)
+        {
+            if (isNeighbour(pos, other.Key))
+            {
+                neighbours[other.Key - pos] = other.Value;
+                other.Value.neighbours[pos - other.Key] = this;
+            }
+        }
+
+        nextAlive = Random.value > 0.5f;
+        Doa();
     }
     
+    
+
+    int Distance(Vector2Int a, Vector2Int b)
+    {
+        return Math.Abs(a.x - b.x) + Math.Abs(a.y - b.y);
+    }
+
+    bool isNeighbour(Vector2Int a, Vector2Int b)
+    {
+        Vector2Int d = a - b;
+        if (d.x == 0 && d.y == 0)
+        {
+            return false;
+        }
+
+        if (Math.Abs(d.x) > 1)
+        {
+            return false;
+        }
+        if (Math.Abs(d.y) > 1)
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
